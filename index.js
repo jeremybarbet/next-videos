5b95cb26b6db8e3f6d0f2084b820473c72e99ac9
Jérémy Barbet <jeremgraph@gmail.com>
2019-04-12 17:27:36 +0000

    Fix missing options obj

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 module.exports = (nextConfig = {}) => {
   return Object.assign({}, nextConfig, {
     webpack(config, options) {
+      const { isServer } = options;
+
       if (!options.defaultLoaders) {
         throw new Error(
           'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
@@ -16,17 +18,17 @@ module.exports = (nextConfig = {}) => {
               fallback: 'file-loader',
               publicPath: `${nextConfig.assetPrefix}/_next/static/videos/`,
               outputPath: `${isServer ? '../' : ''}static/videos/`,
-              name: '[name]-[hash].[ext]'
-            }
-          }
-        ]
+              name: '[name]-[hash].[ext]',
+            },
+          },
+        ],
       });
 
       if (typeof nextConfig.webpack === 'function') {
-        return nextConfig.webpack(config, options)
+        return nextConfig.webpack(config, options);
       }
 
-      return config
-    }
-  })
-}
+      return config;
+    },
+  });
+};
