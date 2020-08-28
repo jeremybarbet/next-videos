61578d0b95d42e576cdfa8a854f0af013de02aa2
Luke Jones <luke-j@users.noreply.github.com>
2020-08-28 21:16:54 +1000

    feat: add types (#7)

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+declare module "*.mp4" {
+  const value: string;
+  export = value;
+}
+
+declare module "*.webm" {
+  const value: string;
+  export = value;
+}
+
+declare module "*.ogg" {
+  const value: string;
+  export = value;
+}
+
+declare module "*.swf" {
+  const value: string;
+  export = value;
+}
+
+declare module "*.ogv" {
+  const value: string;
+  export = value;
+}
